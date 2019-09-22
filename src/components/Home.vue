b20dcc27ac3555a2ec99a4a3cfdfa685796d91f6
Jack Bransfield <bransfieldjack@gmail.com>
2019-09-22 15:17:25 +1000

    UI Update.

---
+++ src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+
+<b-container class="bv-example-row" fluid>
+  <br>
+  <br>
+  <br>
+
+  <b-row>
+    <b-col></b-col>
+    <b-col cols="10">
+      <!-- <img src="https://img.icons8.com/ultraviolet/240/000000/batman-old.png"> -->
+      <hr>
+    </b-col>
+    <b-col></b-col>
+  </b-row>
+  <br>
+  <b-container class="bv-example-row">
+    <b-row>
+      <b-col><a href="https://slack.com/signin"><img src="https://img.icons8.com/ultraviolet/80/000000/slack.png" v-b-popover.hover.top="'Slack Sign in page'"></a></b-col>
+      <b-col><a href="https://www.office.com/"><img src="https://img.icons8.com/ultraviolet/80/000000/windows-10.png" v-b-popover.hover.top="'Office 365 Sign in page'"></a></b-col>
+      <b-col><a href="https://www.linkedin.com/login"><img src="https://img.icons8.com/ultraviolet/80/000000/linkedin.png" v-b-popover.hover.top="'Linkedin Sign in page'"></a></b-col>
+    </b-row>
+  </b-container>
+  <br>
+  <br>
+  <br>
+  <br>
+  <b-container class="bv-example-row">
+    <b-row>
+      <b-col><a href="https://github.com/bransfieldjack"><img src="https://img.icons8.com/ultraviolet/80/000000/code-fork.png" v-b-popover.hover.top="'Github'"></a></b-col>
+      <b-col><a href="https://web.whatsapp.com/"><img src="https://img.icons8.com/ultraviolet/80/000000/whatsapp.png" v-b-popover.hover.top="'Whatsapp'"></a></b-col>
+      <b-col><a href="https://www.reddit.com/login/"><img src="https://img.icons8.com/ultraviolet/80/000000/reddit.png" v-b-popover.hover.top="'Reddit'"></a></b-col>
+    </b-row>
+</b-container>
+
+<br>
+<br>
+<br>
+<br>
+
+</b-container>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <b-navbar class="navbar" fixed="top" toggleable="lg" type="light" variant="primary">
+    <b-navbar-brand href="#"><img src="https://img.icons8.com/ultraviolet/40/000000/ninja-turtle.png" v-b-popover.hover.top="'Home'"></b-navbar-brand>
+    
+
+    <b-navbar-toggle target="nav-collapse" right></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav right>
+    
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <a href=""><img src="https://img.icons8.com/ultraviolet/40/000000/enter-2.png" v-b-popover.hover.top="'Login'"></a>&nbsp;
+      </b-navbar-nav>
+
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
